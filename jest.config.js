84dfb5ac843aa9c9ff10947396465d80ca562f65
nesma011 <nesmaatef349@gmail.com>
2025-10-01 23:04:07 +0300

    test: install jest for test

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
